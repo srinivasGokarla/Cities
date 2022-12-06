99d87457249c3f5c536ad50f0d4a26fc63081345
Gokarla Srinivas <87422030+srinivasGokarla@users.noreply.github.com>
2022-12-06 17:46:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import "./styles.css";
+import { useState } from "react";
+
+/*
+  INSTRUCTIONS:
+  Create a "todo"(add cities) app with the following criteria.
+    1. The user can add new cities
+    2. The user can remove existing cities items
+*/
+
+export default function App() {
+  const [data, setData] = useState("");
+  const [cities, setCities] = useState([]);
+
+  const addCities = (e) => {
+    //Complete function
+    e.preventDefault();
+    if (data != "") {
+      const alldata = { id: new Date().getTime().toString(), name: data };
+      setCities([...cities, alldata]);
+    }
+    setData("");
+  };
+  const RemoveItems = (index) => {
+    const updatecities = cities.filter((ele) => {
+      return index !== ele.id;
+    });
+    setCities(updatecities);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={addCities}>
+        <input
+          type="text"
+          placeholder="Enter city Name"
+          value={data}
+          onChange={(e) => setData(e.target.value)}
+        />
+        <button type="submit">Add</button>
+        <ul>
+          {cities.map((ele) => (
+            <li style={{ margin: "auto" }}>
+              <span key={ele.id}>{ele.name}</span>
+              <button
+                style={{ marginLeft: "10px" }}
+                onClick={() => RemoveItems(ele.id)}
+              >
+                x
+              </button>
+            </li>
+          ))}
+        </ul>
+      </form>
+    </div>
+  );
+}
